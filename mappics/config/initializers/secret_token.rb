d6ea565240147be38063c9ad4ea665266db252ae
Francois Guilbert <francois.guilbert@neoweb.fr>
2014-08-27 10:19:10 +0200

    init

---
+++ mappics/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Mappics::Application.config.secret_key_base = '4c0cd8315e0efd384f76de611f4b2cdabe48039a975b98e602bff83c9da2f4b46bd060c3a28e1a257ae5bfc65f1bfe681343bc9883c653e5195aa8633224c395'
