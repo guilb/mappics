119032ad4ecef01b962620be4b9d4e1b06719bd8
Francois Guilbert <francois.guilbert@neoweb.fr>
2014-09-01 17:38:58 +0200

    config aws

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,6 +1,17 @@
 Mappics::Application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
-
+  ENV['AWS_ACCESS_KEY_ID'] = "AKIAIELVEHTJEIRSJNKQ";
+  ENV['AWS_SECRET_ACCESS_KEY'] = "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";
+  config.paperclip_defaults = {
+    :storage => :s3,
+    :url =>':s3_domain_url',
+    :path => "/:class/:id/:attachment/:style/:filename",
+    :s3_credentials => {
+      :bucket => 'map-pics',
+      :access_key_id => 'AKIAIELVEHTJEIRSJNKQ',
+      :secret_access_key => '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'
+    }
+  }
   # Code is not reloaded between requests.
   config.cache_classes = true
 
