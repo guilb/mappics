81d063b3a9b3b6740137b4be716e8977290e7f85
Francois Guilbert <francois.guilbert@neoweb.fr>
2014-09-02 19:55:18 +0200

    gitignore

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -9,7 +9,7 @@ Mappics::Application.configure do
     :s3_credentials => {
       :bucket => 'map-pics',
       :access_key_id => '',
-      :secret_access_key => '2YIs0hl0KBx7W/J/f/fCRP0+e8vUoUup2tcwR6mQ'
+      :secret_access_key => ''
     }
   }
   # Code is not reloaded between requests.
